3374d8135d4db2a8b039a37437938ee5ca703bf3
kkkddkkk <kindokyeong@gmail.com>
2025-10-17 11:01:11 +0900

    feat: 메인화면 구성

---
+++ frontEnd/moduche/src/pages/Main/Main.js
@@ -0,0 +1,12 @@
+import { Toolbar } from "@mui/material";
+import Layout from "../../component/common/Layout";
+
+const Main=()=>{
+    return(
+        <Layout outer >
+            <Toolbar />
+            
+        </Layout>
+    )
+}
+export default Main;
