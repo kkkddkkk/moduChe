9d8d938aa640d14b7f7dd47dcccd2d64d4de6987
BangSeongnam <qkdtjdska12@gmail.com>
2025-10-17 12:14:15 +0900

    backend 패키지 구성 ing....

---
+++ backEnd/moduche/moduche/src/main/java/com/moduche/config/JpaAuditingConfig.java
@@ -0,0 +1,5 @@
+package com.moduche.config;
+
+public class JpaAuditingConfig {
+
+}
---
+++ backEnd/moduche/moduche/src/test/java/com/moduche/ModucheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moduche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModucheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
