b4903b53331e01436fb61e68d83b11a5e174474a
kkkddkkk <kindokyeong@gmail.com>
2025-10-22 09:36:59 +0900

    fix: layout 세부수정, Text 모든 컴포넌트에 sx 추가, header/footer에 로고 추가

--- frontEnd/moduche/src/component/main/Header.js
+++ frontEnd/moduche/src/component/main/Header.js
@@ -1,104 +1,142 @@
-import { Box, Divider, Grid, IconButton, Typography, useMediaQuery, useTheme } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
+import {
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import { Contents } from "../common/Text";
-import { SlideModal } from "../common/Modals";
-import { useEffect, useRef, useState } from "react";
-import CustomTextField from "../common/CustomTextField";
-import { useNavigate } from "react-router-dom";
-
+import { Contents } from '../common/Text';
+import { SlideModal } from '../common/Modals';
+import { useEffect, useRef, useState } from 'react';
+import CustomTextField from '../common/CustomTextField';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    const theme = useTheme();
-    const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
-    const headerRef = useRef(null);
-    const [openSearch, setOpenSearch] = useState(false);
-    const [headerHeight, setHeaderHeight] = useState(0);
-    const [search, setSearch] = useState("");
-    const navigate = useNavigate();
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up('md'));
+  const headerRef = useRef(null);
+  const [openSearch, setOpenSearch] = useState(false);
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
 
+  const menuColor = theme.palette.primary.main;
 
-    const menuColor = theme.palette.background.default;
-
-    useEffect(() => {
-        if (headerRef.current) {
-            setHeaderHeight(headerRef.current.clientHeight);
-        }
-    }, []);
-
-    const moveTo = ( item ) => {
-        return navigate(`/account/${item}`);
+  useEffect(() => {
+    if (headerRef.current) {
+      setHeaderHeight(headerRef.current.clientHeight);
     }
+  }, []);
 
-    const clickSearchButton = () => {
-        setOpenSearch(!openSearch);
-    }
+  const moveTo = (item) => {
+    return navigate(`/account/${item}`);
+  };
 
-    const HeaderMenu = ({ children, onClick }) => {
-        return (
-            <Contents color={menuColor} hover={true} onClick={onClick}>
-                {children}
-            </Contents>
-        )
-    }
+  const clickSearchButton = () => {
+    setOpenSearch(!openSearch);
+  };
 
-    const MenuBar = () => {
-        return <Divider orientation="vertical" flexItem sx={{ borderColor: menuColor }} />
-    }
-
-    const HeaderIcon = ({ children, onClick }) => {
-        return (
-            <IconButton size="large" edge="end" color="inherit"
-                aria-label="menu" sx={{ mr: 2 }} onClick={onClick}>
-                {children}
-            </IconButton>
-        )
-    }
-
+  const HeaderMenu = ({ children, onClick }) => {
     return (
-        <AppBar position="sticky" ref={headerRef} sx={{ zIndex: 1500 }}>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    모두의 체육관 임시헤더
-                </Typography>
-                <HeaderMenu onClick={clickSearchButton}>검색하기</HeaderMenu>
-                <HeaderIcon onClick={clickSearchButton}>
-                    <SearchIcon />
-                </HeaderIcon>
-                <SlideModal position={"top"} open={openSearch} close={() => setOpenSearch(false)}
-                    headerHeight={headerHeight || 0} title={"검색해보기~"}
-                >
-                    <Grid size={2} />
-                    <Grid size={8}>
-                        <CustomTextField
-                            data={search}
-                            setData={setSearch}
-                            placeholder={"성남님 여기에 퀵서치 디자인해서 넣으시면 될듯요"}
-                            padding={12}
-                        />
-                    </Grid>
-                    <Grid size={2} />
-                </SlideModal>
+      <Contents color={menuColor} hover={true} onClick={onClick} bold>
+        {children}
+      </Contents>
+    );
+  };
 
-                {!isMdUp ?
-                    <HeaderIcon> <MenuIcon /></HeaderIcon> :
-                    <Box
-                        sx={{
-                            display: "flex",
-                            alignItems: "center", // 세로 가운데 정렬
-                            gap: 1, // 항목 간 간격
-                        }}
-                    >
-                        <HeaderMenu onClick={()=>moveTo("login")}>로그인</HeaderMenu>
-                        <MenuBar />
-                        <HeaderMenu onClick={()=>moveTo("joinUs")}>회원가입</HeaderMenu>
-                    </Box>
-                }
+  const MenuBar = () => {
+    return (
+      <Divider
+        orientation="vertical"
+        flexItem
+        sx={{ borderColor: menuColor }}
+      />
+    );
+  };
 
-            </Toolbar>
-        </AppBar >
-    )
-}
+  const HeaderIcon = ({ children, onClick }) => {
+    return (
+      <IconButton
+        size="large"
+        edge="end"
+        color={menuColor}
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={onClick}
+      >
+        {children}
+      </IconButton>
+    );
+  };
+
+  return (
+    <>
+      <AppBar position="sticky" ref={headerRef} sx={{ zIndex: 1500 }}>
+        <Toolbar sx={{ backgroundColor: theme.palette.background.default }}>
+          <Box component={'div'} flexGrow={1}>
+
+              <Box
+                component={'img'}
+                src={`/logo/MODUCHE_LOGO.png`}
+                sx={{
+                  width: '160px',
+                  height: 'auto',
+                  display: 'inline-block',
+                  cursor: 'pointer',
+                }}
+                onClick={()=>navigate("/")}
+              />
+          </Box>
+
+          <HeaderMenu onClick={clickSearchButton}>검색하기</HeaderMenu>
+          <HeaderIcon onClick={clickSearchButton}>
+            <SearchIcon sx={{ color: menuColor, fontWeight: 'bold' }} />
+          </HeaderIcon>
+          {!isMdUp ? (
+            <HeaderIcon>
+              {' '}
+              <MenuIcon />
+            </HeaderIcon>
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center', // 세로 가운데 정렬
+                gap: 1, // 항목 간 간격
+              }}
+            >
+              <HeaderMenu onClick={() => moveTo('login')}>로그인</HeaderMenu>
+              <MenuBar />
+              <HeaderMenu onClick={() => moveTo('joinUs')}>회원가입</HeaderMenu>
+            </Box>
+          )}
+        </Toolbar>
+      </AppBar>
+      <SlideModal
+        position={'top'}
+        open={openSearch}
+        close={() => setOpenSearch(false)}
+        headerHeight={headerHeight || 0}
+        title={'검색해보기~'}
+      >
+        <Grid size={2} />
+        <Grid size={8}>
+          <CustomTextField
+            data={search}
+            setData={setSearch}
+            placeholder={'성남님 여기에 퀵서치 디자인해서 넣으시면 될듯요'}
+            padding={12}
+          />
+        </Grid>
+        <Grid size={2} />
+      </SlideModal>
+    </>
+  );
+};
 export default Header;
